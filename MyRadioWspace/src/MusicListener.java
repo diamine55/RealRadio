e8219d87ef997092d1ef41f6fad4fb6d5ef3875b
Matt <mdmichli@buffalo.edu>
2014-07-22 18:34:15 -0400

    we fucked up

--- MyRadioWspace/src/MusicListener.java
+++ MyRadioWspace/src/MusicListener.java
@@ -4,7 +4,6 @@ import java.awt.event.KeyListener;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 
-
 public class MusicListener implements KeyListener {
 
 	private JMusicPanel MP = Main.getMusPan();
@@ -12,43 +11,44 @@ public class MusicListener implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_DOWN && MP.selectY < 203) {
-			//System.out.println("Moar FuckNuggets");
+			// System.out.println("Moar FuckNuggets");
 			MP.line = 20;
 			MP.selectY += 20;
 			MP.repaint();
-			//System.out.println(MP.selectY);
+			// System.out.println(MP.selectY);
 		}
 		if (e.getKeyCode() == KeyEvent.VK_UP && MP.selectY > 3) {
-		//	System.out.println("Mas FuckNugatos");
+			// System.out.println("Mas FuckNugatos");
 			MP.line = 20;
 			MP.selectY -= 20;
 			MP.repaint();
-			//System.out.println(MP.selectY);
+			// System.out.println(MP.selectY);
 		}
-<<<<<<< HEAD
+
 		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-			int whattt = (MP.selectY-3)/20;
+			int whattt = (MP.selectY - 3) / 20;
 			System.out.println(whattt);
-		///	System.out
-			///		.println("Bet you expected fucknuggets...Nope. Chuck Testa");
-=======
+			// / System.out
+			// / .println("Bet you expected fucknuggets...Nope. Chuck Testa");
+		}
+
 		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			Main.frame.remove(Main.frame.findComponentAt(10, 10));
 			Main.mPanel.setBackground(Color.black);
 			Main.frame.removeKeyListener(Main.NL);
-			
-			KeyListener musicLis = new MusicListener();	//Need to make new key listener
-			Main.frame.addKeyListener(musicLis);	   	//
+
+			KeyListener musicLis = new MusicListener(); // Need to make new
+														// key listener
+			Main.frame.addKeyListener(musicLis); //
 			Main.frame.add(Main.jmPanel);
 
 			Main.frame.revalidate();
 			Main.jmPanel.loadSong(null);
 			System.out.println("worked");
->>>>>>> origin/master
 		}
 
 	}
-	
+
 	public void getPath(){
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -75,7 +75,7 @@ public class MusicListener implements KeyListener {
 
 			c.close();
 			System.out.println("Database Connection Closed");
-	}
+		}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
